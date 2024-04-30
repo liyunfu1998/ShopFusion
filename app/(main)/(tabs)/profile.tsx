57d1fb5477a5b4ff0c1acb5121deb5a02fd7009e
李云富 <liyunfu1998@qq.com>
2024-04-30 06:37:29 +0000

    feat: add tailwindcss

--- app/(main)/(tabs)/profile.tsx
+++ app/(main)/(tabs)/profile.tsx
@@ -15,7 +15,7 @@ export default function Profile() {
   };
 
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-gray-500">
       <Text>Profile Screen</Text>
       <Pressable onPress={onLogIn}>
         <Text>Log In</Text>
